6d18428c918013ef9fba03b391c990fc2e0b7efa
Константин Морозов <morozov.k@phystech.edu>
2020-04-18 15:16:39 +0300

    silver league ~ 300

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <set>
 #include <random>
+#include <sstream>
 
 using namespace std;
 
@@ -51,6 +52,15 @@ vector<vector<int> > get_sector_mask(int sector_index) {
     return mask;
 }
 
+int sum_mask(const vector<vector<int> >& mask) {
+    int sum = 0;
+    for (auto el : mask) {
+        for (auto e : el) {
+            sum += e;
+        }
+    }
+    return sum;
+}
 
 void shift_up(vector<vector<int> > &mask) {
     for (int i = 0; i < width; ++i) {
@@ -123,7 +133,7 @@ pair<int, int> shoot_torpedo(int x, int y, int depth, set<pair<int, int> > &used
         if (var.first == -1 || !enemy_mask[var.first][var.second]) {
             continue;
         } else {
-            if (max(abs(var.first - my_x), abs(var.second - my_y)) < 2) {
+            if ((max(abs(var.first - my_x), abs(var.second - my_y)) < 2 && sum_mask(enemy_mask) > 1)) {
                 continue;
             }
             return var;
@@ -140,6 +150,18 @@ pair<int, int> choose_begin_point() {
             }
         }
     }
+}
+
+void fill_torpedo_mask(int x, int y, int depth, vector<vector<int> > &mask) {
+    if (x < 0 || y < 0 || x == width || y == height ||
+        depth > TORPEDO_DEPTH || !field_mask[x][y] || mask[x][y]) {
+        return;
+    }
+    mask[x][y] = 1;
+    fill_torpedo_mask(x + 1, y, depth + 1, mask);
+    fill_torpedo_mask(x - 1, y, depth + 1, mask);
+    fill_torpedo_mask(x, y + 1, depth + 1, mask);
+    fill_torpedo_mask(x, y - 1, depth + 1, mask);
 }
 
 void analyze_enemy(const string &command) {
@@ -161,11 +183,11 @@ void analyze_enemy(const string &command) {
                 shift_right(enemy_mask);
             }
         }
-        if (command.find(surface_command, i) == i) {
+        else if (command.find(surface_command, i) == i) {
             int sector_index = command[command.find(surface_command, i) + surface_command.size() + 1] - '0';
             and_mask(enemy_mask, get_sector_mask(sector_index));
         }
-        if (command.find(silence_command, i) == i) {
+        else if (command.find(silence_command, i) == i) {
             vector<vector<int> > n, s, e, w;
             n = s = e = w = enemy_mask;
             for (int j = 1; j <= 4; ++j) {
@@ -179,8 +201,27 @@ void analyze_enemy(const string &command) {
                 or_mask(enemy_mask, n);
             }
         }
+        else if (command.find(torpedo_command, i) == i) {
+            stringstream in(command.substr(command.find(' ', i) + 1, string::npos));
+            int x, y;
+            in >> x >> y;
+            auto torpedo_mask = get_sector_mask(-1);
+            fill_torpedo_mask(x, y, 0, torpedo_mask);
+            and_mask(enemy_mask, torpedo_mask);
+        }
 
         while(i < command.size() && command[i] >= 'A' && command[i] <= 'Z') ++i;
+    }
+    if (sum_mask(enemy_mask) == 1) {
+        for (int i = 0; i < width; ++i) {
+            for (int j = 0; j < height; ++j) {
+                if (enemy_mask[i][j]) {
+                    cout << "MSG " << i << " " << j << "|";
+                }
+            }
+        }
+    } else {
+        cout << "MSG " << sum_mask(enemy_mask) << "|";
     }
 }
 
@@ -228,21 +269,34 @@ char select_move() {
     return pos.second;
 }
 
-string make_my_command(int torpedoCooldown) {
-    if (torpedoCooldown == 0) {
+string make_my_command(int torpedoCooldown, int silenceCooldown) {
+    string power = torpedo_command, move = move_command, step;
+
+    if (torpedoCooldown == 0 && sum_mask(enemy_mask) < 20) {
         set<pair<int, int> > used;
         auto possible_shoot = shoot_torpedo(my_x, my_y, 0, used);
         if (possible_shoot.first != -1) {
-            return torpedo_command + " " + to_string(possible_shoot.first) + " " + to_string(possible_shoot.second);
+            cout << torpedo_command + " " + to_string(possible_shoot.first) + " " + to_string(possible_shoot.second)
+                 << "|";
         }
     }
+
+    if (torpedoCooldown == 0) {
+        power = silence_command;
+    }
+
+    if (silenceCooldown == 0) {
+        move = silence_command;
+        power = "1";
+    }
+
     char move_direction = select_move();
     if (move_direction == '?') {
         my_mask = field_mask;
         my_mask[my_x][my_y] = 0;
         return surface_command;
     }
-    return move_command + " " + move_direction + " " + torpedo_command;
+    return move + " " + move_direction + " " + step + power;
 }
 
 int main()
@@ -283,6 +337,6 @@ int main()
         string opponentOrders;
         getline(cin, opponentOrders);
         analyze_enemy(opponentOrders);
-        cout << make_my_command(torpedoCooldown) << endl;
+        cout << make_my_command(torpedoCooldown, silenceCooldown) << endl;
     }
 }
